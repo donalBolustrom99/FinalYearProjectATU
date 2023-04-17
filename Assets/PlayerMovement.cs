529b0093f0e679cc77337ac2e5fbb4ac86f2649d
donalbolustrom99 <donalbolustrom1999@gmail.com>
2023-04-17 02:24:37 +0100

    Jumping Added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,13 +7,21 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float rotationSpeed;
+    public float jumpSpeed;
+    public float jumpGrace;
 
     private CharacterController cC;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpPressed;
 
     // Start is called before the first frame update
     void Start()
     {
         cC = GetComponent<CharacterController>();
+
+        originalStepOffset = cC.stepOffset;
     }
 
     // Update is called once per frame
@@ -26,7 +34,38 @@ public class PlayerMovement : MonoBehaviour
         float magnitude = Mathf.Clamp01(moveDirection.magnitude)*speed;
         moveDirection.Normalize();
 
-        cC.SimpleMove(moveDirection * magnitude);
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (cC.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = Time.time;
+        }
+
+
+        if (Time.time - lastGroundedTime <= jumpGrace)
+        {
+            cC.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+            if (Time.time - jumpPressed <= jumpGrace)
+            {
+                ySpeed = jumpSpeed;
+                jumpPressed = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            cC.stepOffset = 0;
+        }
+        
+        Vector3 velocity = moveDirection * magnitude;
+        velocity.y = ySpeed;
+        cC.Move(velocity * Time.deltaTime);
 
         if(moveDirection != Vector3.zero)
         {
