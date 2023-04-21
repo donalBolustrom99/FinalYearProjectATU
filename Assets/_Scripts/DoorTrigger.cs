88d7fec3f5838a047a4e70fe2789923776609867
donalbolustrom99 <donalbolustrom1999@gmail.com>
2023-04-21 04:05:08 +0100

    Level 2 Base

---
+++ Assets/_Scripts/DoorTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+
+    bool isOpen = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isOpen == false)
+        {
+            door.transform.position += new Vector3(0, 4, 0);
+            isOpen = true;
+        }
+    }
+}
